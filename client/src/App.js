3b155af80ad7cb2aedf87aa88b3ab7d400655c67
John Robling <roblingjohn@gmail.com>
2020-08-07 13:24:57 -0400

    set up routes for pages, created components for header and footer with placeholders

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,42 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import Home from './containers/Home/Home';
-
-
-
+import React, { useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./containers/Home/Home";
+import Leaderboard from "./containers/Leaderboard/Leaderboard";
+import Login from "./containers/Login/Login";
+import Quiz from "./containers/Quiz/Quiz";
+import SelectQuiz from "./containers/SelectQuiz/SelectQuiz";
+import Signup from "./containers/Signup/Signup";
+import UpdateUser from "./containers/UpdateUser/UpdateUser";
+import User from "./containers/User/User";
+import NoMatch from "./containers/NoMatch/NoMatch";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-
-
-
   useEffect(() => {
-    axios
-      .get("/api/config")
-      .then((res) => {
-        console.log("Testing backend sending data", res.data)
-      }).catch((err) => {
-        console.log(err)
-      })
-
-  })
-
+    axios.get("/api/config").then((res) => {
+      console.log(res.data);
+    });
+  });
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/leaderboard" component={Leaderboard} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/quiz" component={Quiz} />
+          <Route exact path="/selectquiz" component={SelectQuiz} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/updateuser" component={UpdateUser} />
+          <Route exact path="/user" component={User} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>This is the navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
