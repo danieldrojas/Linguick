76f623bc97f741e3e73fa612a86e84ce28d014b0
Sharon Kim <sharonkim1709@gmail.com>
2020-08-10 10:27:09 -0400

    modified schema for array prop

--- models/Language.js
+++ models/Language.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const LanguageSchema = new Schema({
-  language_name:{
-      type:String,
-      required:"language is required"
+  language_name: {
+    type: String,
+    required: "language is required",
   },
-  characters:{
-      type:String,
-      required:"character is required"
-  }
+  characters: {
+    type: String,
+    required: "character is required",
+  },
 });
 
 const Language = mongoose.model("Language", LanguageSchema);
--- models/Question.js
+++ models/Question.js
@@ -3,21 +3,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const QuestionSchema = new Schema({
-  question: {
-    type:Array,
-    required:true
-  }
-  ,
-  choices: {
-    type:Array,
-    required:true
-  }
-  ,
-  answers: {
-    type:Array,
-    required:true
-  }
-  ,
+  question: [
+    {
+      type: String,
+    },
+  ],
+  choices: [
+    {
+      type: String,
+    },
+  ],
+  answers: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
 });
 
 const Question = mongoose.model("Question", QuestionSchema);
