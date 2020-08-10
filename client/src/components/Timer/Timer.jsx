10115775c095236ad544f1d16a85a8310b4ffa3b
Sam Dratch <dratch.sam1@gmail.com>
2020-08-10 14:16:22 -0400

    seperated out timer component

--- client/src/components/Timer/Timer.jsx
+++ client/src/components/Timer/Timer.jsx
@@ -2,31 +2,23 @@ import React, { useState, useEffect } from "react";
 //import { BrowserRouter as Router, Redirect, Link } from "react-router-dom";
 
 const Timer = (props) => {
-  // const [timer, setTimer] = useState([]);
-  // const [timerActive, setTimerActive] = useState([]);
+   const [timer, setTimer] = useState(100);
 
-  // useEffect(() => {
-  //   setTimerActive(true);
-  //   let timerCount = props.time;
-  //   const interval = setInterval(() => {
-  //     setTimer(timerCount);
-  //     timerCount = timerCount - 1;
-  //     if (timerCount < 0) {
-  //       setTimerActive(false);
-  //       clearInterval(interval);
-  //       // return <Redirect push to="./user" />;
-  //     }
-  //   }, 1000);
-  // }, []);
-  //   const timerStop = () => {}
+  useEffect(() => {
+    let timerCount = timer
+    const interval = setInterval(() => {
+      timerCount--
+      setTimer(timerCount);
+      if (timerCount <= 0) {
+        clearInterval(interval);
+        window.location.href = "/leaderboard"
+      }
+    }, 1000);
+  }, [timer]);
+
   return (
     <div>
-      {/* <h3>{timer}</h3>
-      {timerActive === false && (
-        <Link to="/user">
-          <button>See results</button>
-        </Link>
-      )} */}
+      <h3>{timer}</h3>
     </div>
   );
 };
--- client/src/containers/Quiz/Quiz.jsx
+++ client/src/containers/Quiz/Quiz.jsx
@@ -1,28 +1,21 @@
 import React, {Component} from "react";
 //import Timer from '../../components/Timer/Timer'
 import question from "./testQuestions.json"
+import Timer from "../../components/Timer/Timer"
 
-let timer; 
 class Quiz extends Component{
   state = {
     quiestion:"",
     choices: [],
     answer:"",
     index:0,
-    time:5000,
   };
   
-
+//Start timer and get 1st question from database
   componentDidMount(){
     this.setState(question[0]) 
-    timer = setInterval( () => {
-      this.setState({time:this.state.time-1})
-      if(this.state.time <= 0){
-        clearInterval(timer)
-        window.location.href = "/leaderboard"
-      }
-    },10)
   }
+
   handleButtonPress = (event) => {
     event.preventDefault();
     //check if the guess is correct
@@ -32,7 +25,6 @@ class Quiz extends Component{
       //handling for when the game is completed
       if(question.length === this.state.index+1){
         console.log("You completed the game")
-        clearInterval(timer)
         window.location.href = "/leaderboard"
       }
       //update the page with the next set of questions
@@ -43,14 +35,14 @@ class Quiz extends Component{
     //if guess incorrectly
     else{
       console.log("You Guessed Incorrectly")
-      this.setState({time:this.state.time - 300})
     }
   };
+
 
   render(){
     return (
       <container>
-        <h2>{this.state.time}</h2>
+        <Timer wrong = {this.setState.isWrong}/>
         <h1>Question: {this.state.question}</h1>
         <button onClick = {this.handleButtonPress} id = {this.state.choices[0]}>{this.state.choices[0]}</button>
         <button onClick = {this.handleButtonPress} id = {this.state.choices[1]}>{this.state.choices[1]}</button>
