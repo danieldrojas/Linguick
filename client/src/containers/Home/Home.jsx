80b835947e712aafc3b9ead0ec80f41fde7ce88c
John Robling <roblingjohn@gmail.com>
2020-08-10 12:14:10 -0400

    fixed typo on home page

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <container>
-      <h1>Get ready to test your skills with the Korean alpahbet!</h1>
+      <h1>Get ready to test your skills with the Korean alphabet!</h1>
       <Link to="/quiz">
         <button>Get started!</button>
       </Link>
