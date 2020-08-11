a3f69cf80debbc6cef2f668402628ad4eeeb8f94
John Robling <roblingjohn@gmail.com>
2020-08-11 13:40:13 -0400

    refactored leaderboard into table

--- client/src/components/HighScoreEl/HighScoreEl.jsx
+++ client/src/components/HighScoreEl/HighScoreEl.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const HighScoreEl = (props) => {
-    return (
-        <li>{props.username}: {props.score}</li>
-    );
+  return (
+    <tr>
+      <td>{props.username}</td>
+      <td>{props.quizName}</td>
+      <td>{props.score}</td>
+    </tr>
+  );
 };
 
 export default HighScoreEl;
