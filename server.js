8c096435fc6b2b388dcbb644c40ce41e9e22d6f5
Daniel Rojas <daniel.drojas@outlook.com>
2020-08-11 11:34:32 -0400

    added middleware to server

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 app.use(express.static("client/build"));
 
 app.use(routes)
