96822843be27f7d9f2cada46e40e6ea16a8f1000
Daniel Rojas <daniel.drojas@outlook.com>
2020-08-12 08:23:49 -0400

    created a userContext in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./containers/Home/Home";
@@ -12,16 +12,43 @@ import User from "./containers/User/User";
 import NoMatch from "./containers/NoMatch/NoMatch";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import UserContext from "./util/UserContext";
+import API from "./util/API";
+
 
 function App() {
-  // useEffect(() => {
-  //   axios.get("/api/config").then((res) => {
-  //     console.log(res.data);
-  //   });
-  // });
+
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    total_scores: 91,
+  })
+
+  useEffect(() => {
+
+    loadUsers()
+  
+  }, []);
+
+
+   
+
+  function loadUsers() {
+    API.getUserInfo("5f32d8a63ee8e5c604b93ac1")
+      .then((res) => {
+        console.log("testing user from db: ", res)
+        setUser(res)
+    })
+  }
+  
+  //with email with can find the user in data base to access database info
+
+  
   return (
     <>
       <Router>
+        <UserContext.Provider value={user}>
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
@@ -33,7 +60,8 @@ function App() {
           <Route exact path="/updateuser" component={UpdateUser} />
           <Route exact path="/user" component={User} />
           <Route component={NoMatch} />
-        </Switch>
+          </Switch>
+        </UserContext.Provider>
       </Router>
       <Footer />
     </>
