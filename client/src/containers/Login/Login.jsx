9549279a2ef9c1cd40d69a9431db25c139e58029
John Robling <roblingjohn@gmail.com>
2020-08-13 11:21:55 -0400

    reformatted login page

--- client/src/containers/Login/Login.jsx
+++ client/src/containers/Login/Login.jsx
@@ -1,53 +1,67 @@
-import React, { useState} from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import API from "../../util/API";
 
 const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log("front end username is " + email);
     console.log("front end password is " + password);
 
     API.getUserLogin({
-      email
+      email,
     })
       .then((dbUser) => {
-        console.log("this is my dbUser ", dbUser.data)
+        console.log("this is my dbUser ", dbUser.data);
         if (!dbUser.error) {
           // dbUser.data.password === password ? props.history.push : alert("Email or password invalid")
-       props.history.push("./Selectquiz")
+          props.history.push("./Selectquiz");
         }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-      }).catch(err => {
-        console.log(err)
-      })  
-}
-  
   return (
     <div className="container">
       <h1>Log in</h1>
-      <form onSubmit={handleSubmit}>
-          <ul>
-          <li>Email: <input
-            type="text"
-            name="email"
-            value={email}
-            placeholder="Email"
-            onChange={e => setEmail(e.target.value)}
-
-          /></li>
-          <li>Password: <input type="password"
-            name="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}/></li>
-        </ul>
-        <button>Log In</button>
-      </form>
-      <p>Don't have an account? <Link to="/signup">Sign up.</Link> </p>
+      <div class="row">
+        <div className="col s3"></div>
+        <div className="col s6 signupCol">
+          <form onSubmit={handleSubmit}>
+            <ul>
+              <li>
+                {" "}
+                <input
+                  type="text"
+                  name="email"
+                  value={email}
+                  placeholder="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </li>
+              <li>
+                {" "}
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </li>
+            </ul>
+            <button>Log In</button>
+          </form>
+        </div>
+      </div>
+      <p>
+        Don't have an account? <Link to="/signup">Sign up.</Link>{" "}
+      </p>
     </div>
   );
 };
