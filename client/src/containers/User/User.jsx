2d4d9e4d8e7246a5de81e37f2e03aaac7225d8e9
John Robling <roblingjohn@gmail.com>
2020-08-13 10:37:21 -0400

    added button on user page to take another quiz, changed leaderboard link on user page to button

--- client/src/containers/User/User.jsx
+++ client/src/containers/User/User.jsx
@@ -7,31 +7,35 @@ import API from "../../util/API";
 class User extends Component {
   state = {
     user: {
-      quizzes_taken: []
-    }
+      quizzes_taken: [],
+    },
   };
 
   componentDidMount() {
     // change to getUser when we have authentification worked out
     API.getAllUsers().then((res) => {
-      this.setState({user: res.data[1]})
-      console.log(this.state.user.quizzes_taken)
-    }
-    )
+      this.setState({ user: res.data[1] });
+      console.log(this.state.user.quizzes_taken);
+    });
   }
 
   render() {
     return (
       <div className="container">
         <h1>Welcome to your dashboard, {this.state.user.username}</h1>
-        <Link to="/leaderboard">See world rankings</Link>
+        <Link to="/leaderboard">
+          <button class="sq-btn">See World Rankings</button>
+        </Link>
+        <Link to="/selectquiz">
+          <button class="sq-btn">Take Another Quiz</button>
+        </Link>
         <h2>Quizzes Taken:</h2>
         <tbody className="table">
           <tr>
             <th>Quiz Name</th>
             <th>Score</th>
           </tr>
-          {this.state.user.quizzes_taken.map(quiz => (
+          {this.state.user.quizzes_taken.map((quiz) => (
             <UDQuizSore quizName={quiz.quizName} score={quiz.score} />
           ))}
         </tbody>
