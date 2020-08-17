a9add7937f952de3e037b411f1e8d2a9d1793872
John Robling <roblingjohn@gmail.com>
2020-08-16 21:23:21 -0400

    set leaderboard to only display one score from each user

--- client/src/components/QuizTables/QuizTables.jsx
+++ client/src/components/QuizTables/QuizTables.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import HighScoreEl from "../HighScoreEl/HighScoreEl";
 
 const QuizTables = (props) => {
-
   let filteredArray = [];
   filteredArray = props.scoreArray.filter(
     (quiz) => quiz.newQuizName === props.quizName
   );
+  let userArray = [];
+  userArray = [...new Set(filteredArray.map((item) => item.newUsername))];
+  let singleUserArray = [];
+  for (let i=0; i<userArray.length; i++) {
+    let newEntry = filteredArray.find(e => e.newUsername === userArray[i])
+    singleUserArray.push(newEntry)
+  }
   return (
     <div>
       <h3>{props.quizName}</h3>
@@ -17,12 +23,12 @@ const QuizTables = (props) => {
             <th>Username</th>
             <th>Score</th>
           </tr>
-          {filteredArray.map((score) => (
+          {singleUserArray.map((score) => (
             <HighScoreEl
               quizName={props.quizName}
               username={score.newUsername}
               score={score.newScore}
-              rank={filteredArray.indexOf(score) + 1}
+              rank={singleUserArray.indexOf(score) + 1}
             />
           ))}
         </tbody>
